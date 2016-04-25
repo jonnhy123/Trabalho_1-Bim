7ecf6fa22a42af5be3680e3c27ab3484081328b5
Jhonatan Mattana <jhonatan-mattana@hotmail.com>
2016-04-25 14:31:58 -0300

    Concluindo implementação da class Cliente

--- TudoAqui/br/univel/Cliente.java
+++ TudoAqui/br/univel/Cliente.java
@@ -6,4 +6,19 @@ public class Cliente {
 	@Coluna(pk = true)
 	private int id;
 	
+	@Coluna(nome = "NOME", tamanho = 15)
+	private String nome;
+	
+	@Coluna(nome = "ESTADO_CIVIL", tamanho = 15)
+	private EstadoCivil estadoCivil;
+	
+	@Coluna(nome = "TELEFONE", tamanho = 12)
+	private int telefone;
+	
+	@Coluna(nome = "IDADE", tamanho = 3)
+	private int idade;
+	
+	@Coluna(nome = "ENDERECO", tamanho = 20)
+	private String endereco;
+	
 }
