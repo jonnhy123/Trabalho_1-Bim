23e7989dfa4ffb1cb5386e24b6633556d05bca27
Jhonatan Mattana <jhonatan-mattana@hotmail.com>
2016-04-25 16:07:13 -0300

    Implementando conexao com H2

--- TudoAqui/br/univel/ConexaoComBanco.java
+++ TudoAqui/br/univel/ConexaoComBanco.java
@@ -1,5 +1,16 @@
 package br.univel;
 
+import java.sql.Connection;
+
 public class ConexaoComBanco {
 
+	private Connection con;
+	private static ConexaoComBanco conDB;
+	private String url = "jdbc:h2:~/test";
+	private String driver = "org.h2.Driver";
+	private String senha = "sa";
+	private String usuario = "sa";
+	
+	
+	
 }
